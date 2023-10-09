753cdd0bd001c088acfcf2ef069e4fbfe79e9d75
SVitaliiR <vitaliyruslanov@gmail.com>
2023-10-09 21:26:03 +0300

    Final

---
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.graphics.Color;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView MainText;
+    private Button ColorChangeButton;
+    private boolean textColorChanged = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize views
+        MainText = findViewById(R.id.MainText);
+        ColorChangeButton = findViewById(R.id.ColorChangeButton);
+    }
+
+    public void ChangeMainTextTxt(View view) {
+        MainText.setText("Text has changed");
+    }
+
+    public void ChangeMainTextColor(View view) {
+        if (textColorChanged) {
+            MainText.setTextColor(Color.BLACK);
+        } else {
+            MainText.setTextColor(Color.RED);
+        }
+        textColorChanged = !textColorChanged;
+        Toast.makeText(this, "Color Has Changed", Toast.LENGTH_SHORT).show();
+    }
+}
